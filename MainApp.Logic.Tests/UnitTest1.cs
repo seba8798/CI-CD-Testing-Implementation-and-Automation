769a492faf42b84bcbf7e73867a3122c4420c3fb
seba8798 <seba8798@edu.sde.dk>
2025-08-06 10:50:55 +0200

    new

--- MainApp.Logic.Tests/UnitTest1.cs
+++ MainApp.Logic.Tests/UnitTest1.cs
@@ -11,8 +11,8 @@ public class Class1Tests
         // Arrange
         var logic = new Class1();
         // Act
-        var result = logic.Add(1, 4);
+        var result = logic.Add(1, 6);
         // Assert
-        Assert.Equal(5, result);
+        Assert.Equal(7, result);
     }
 }
