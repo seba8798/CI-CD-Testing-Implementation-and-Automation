3522eabf5e57c86d9e3cbe9f3c501f346b46be29
Toftegaard1996 <alliwantisthegoodlife@gmail.com>
2025-08-06 12:07:20 +0200

    QA test succes push

---
+++ MainApp.Logic.Tests/QATest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp.Logic.Tests
+{
+    public class QATest1
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSumNewQA0()
+        {
+            // Arrange
+            var logic = new Class1();
+            // Act
+            var result = logic.Add(1, 6);
+            // Assert
+            Assert.Equal(7, result);
+        }
+    }
+}
