a9709362102c8bbe32835d3c65e75796deb36fcd
Toftegaard1996 <alliwantisthegoodlife@gmail.com>
2025-08-06 12:08:59 +0200

    QA Test failure

--- MainApp.Logic.Tests/QATest1.cs
+++ MainApp.Logic.Tests/QATest1.cs
@@ -14,7 +14,7 @@ namespace MainApp.Logic.Tests
             // Arrange
             var logic = new Class1();
             // Act
-            var result = logic.Add(1, 6);
+            var result = logic.Add(19, 6);
             // Assert
             Assert.Equal(7, result);
         }
